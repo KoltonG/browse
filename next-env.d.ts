b1577f721c54ec409de7b4d49b48d14f8461f825
Kolton <Kolton.Gagnon@Gmail.com>
2021-03-19 00:27:59 -0300

    Update project to use SASS, Prettier and custom Tailwind config

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,50 +3,94 @@
 
 // Custom Handsfree type definition
 namespace HandsfreeModule {
-  interface Plugin {
-    palmPointers: {
-      enabled: boolean | function;
-      enable?: function;
-      showPointers?: function;
-      pointer: { x: number; y: number }[4];
-      // How much to offset the pointers by
-      // - This is useful for when the camera won't be in front of you
-      // - This is also useful when working with multiple displays
-      offset?: { x: number; y: number };
-      // A multiplier to apply to moving the pointer
-      speed?: { x: number; y: number };
-    };
+  // Base plugin
+  type Plugin = {
+    // Enable the plugin during instantiation
+    enabled: boolean
+    // Enable the plugin after instantiation
+    enable: function
+  }
+  // Palm Pointer plugin
+  type PluginPalmPointer = Plugin & {
+    showPointers?: function
+    pointer: { x: number; y: number }[4]
+    // How much to offset the pointers by
+    // - This is useful for when the camera won't be in front of you
+    // - This is also useful when working with multiple displays
+    offset?: { x: number; y: number }
+    // A multiplier to apply to moving the pointer
+    speed?: { x: number; y: number }
+  }
+  // Pinch Scroll plugin
+  type PluginPinchScroll = Plugin & {
+    // TODO: Add missing types
+  }
+
+  type Plugins = {
+    palmPointers: PluginPalmPointer
+    pinchScroll: PluginPinchScroll
+  }
+
+  type Gesture = {
+    // The name of the `gesture` to create. If the name is taken then this will // overwrite the existing gesture.
+    name: string
+    algorithm: 'fingerpose'
+    models: 'hands'
+    // Between 0 and 10
+    confidence: string
+    // A JSON object which describes the gesture.
+    description: [string, string, string, number][]
   }
 
   export class Handsfree {
-    plugin: Plugin;
+    plugin: Plugins
 
     constructor(options: {
       hands:
         | boolean
         | {
-            enabled: boolean;
+            enabled: boolean
             // The maximum number of hands to detect [0 - 4]
-            maxNumHands?: 1 | 2 | 3 | 4;
+            maxNumHands?: 1 | 2 | 3 | 4
             // Minimum confidence range [0 - 1] for a hand to be considered detected
-            minDetectionConfidence?: number;
+            minDetectionConfidence?: number
             // Minimum confidence range [0 - 1] for the landmark tracker to be considered detected
             // Higher values are more robust at the expense of higher latency
-            minTrackingConfidence?: number;
-          };
-      weboji?: boolean;
-      plugin?: Plugin;
-    });
-    start: () => void;
+            minTrackingConfidence?: number
+          }
+      weboji?: boolean
+      // Plugin settings
+      plugin?: Plugins
+    })
+
+    start: () => void
+
+    /** Adds a new gesture to Handsfree */
+    useGesture(gesture: Gesture)
+
+    /**
+     * When you handsfree.use() you'll have the option of setting its tags.
+     * When running handsfree.disablePlugins() it will search through all
+     * plugins and disable the ones that contain any of those tags. If no tags
+     * are passed then all plugins are disabled.
+     *
+     * This can be used along with handsfree.enablePlugins() to instantly
+     * swap out entire user experiences by tag.
+     *
+     * @param tag A tag or list of tags of all plugins to disable. Pass null
+     * to disable them all.
+     * @link https://handsfree.js.org/ref/method/disablePlugins.html#method-handsfree-disableplugins
+     */
+    disablePlugins(tag?: string | string[])
   }
 }
 
 // Expanding the Window object to include handsfree
 interface Window {
-  handsfree: HandsfreeModule.Handsfree;
+  handsfree: HandsfreeModule.Handsfree
 }
 
 // Custom typing for Handsfree
-declare module "handsfree" {
-  export default HandsfreeModule.Handsfree;
+declare module 'handsfree' {
+  export default HandsfreeModule.Handsfree
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,45 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      darkPink: '#66273D',
+      pink: '#EF4B85',
+      orange: '#FEBE19',
+      white: '#FEFDFD',
+      grey: '#484848'
+    },
+    screens: {
+      desktop: '1280px'
+    },
+    spacing: generateSpacing()
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
+}
+
+/**
+ * Utility to generate incremental spacing.
+ *
+ * Example: Using increment of 8 will output:
+ * {
+ *  0.5: 4px
+ *  1:   8px,
+ *  2:  16px,
+ *  3:  24px,
+ *  ...
+ * }
+ * */
+function generateSpacing(increment = 8) {
+  return {
+    ...[0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reduce(
+      (acc, i) => ({
+        ...acc,
+        // Handles removal of floats. Ref: https://stackoverflow.com/a/8388483
+        [i]: `${(increment * i) | 0}px`
+      }),
+      {}
+    )
+  }
 }
